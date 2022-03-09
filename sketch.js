5f8778b8f0d342beeca84fa8d43170ea685e0210
Divyakallepalli <89527241+Divyakallepalli@users.noreply.github.com>
2022-03-09 22:43:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var PLAY = 1;
+var END  = 0;
+//load images and animations
+function preload(){
+  
+  
+  
+}
+
+//create sprites
+
+function setup() {
+  createCanvas(400, 400);
+  
+
+
+  
+
+   
+ }
+//code
+
+function draw() {
+background(23,165,137);
+  
+
+  
+drawSprites();
+  
+}
+
+
+  
